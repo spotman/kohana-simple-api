b3c001d9c7fdd68096eea9ef3ba04db2acc1425d
Spotman <i.am@spotman.ru>
2018-09-28 13:27:39 +0300

    Make ApiMethodException messages public

--- classes/Spotman/Api/ApiMethodException.php
+++ classes/Spotman/Api/ApiMethodException.php
@@ -1,4 +1,16 @@
 <?php
 namespace Spotman\Api;
 
-class ApiMethodException extends ApiException {}
+class ApiMethodException extends ApiException
+{
+    /**
+     * If returns true, then original exception message will be shown to end-user in JSON and error pages
+     * Override this method with *true* return if it's domain exception
+     *
+     * @return bool
+     */
+    public function showOriginalMessageToUser(): bool
+    {
+        return true;
+    }
+}
