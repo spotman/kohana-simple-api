b65b4e249cfd5570a033ffeb86cc4fd3f186cc83
Denis Terekhov <i.am@spotman.ru>
2024-04-25 15:20:51 +0300

    Fix php8 compatibility

--- classes/Spotman/Api/ApiMethodResponse.php
+++ classes/Spotman/Api/ApiMethodResponse.php
@@ -59,7 +59,7 @@ final class ApiMethodResponse implements JsonSerializable
      * which is a value of any type other than a resource.
      * @since 5.4.0
      */
-    public function jsonSerialize()
+    public function jsonSerialize(): mixed
     {
         return $this->asArray();
     }
